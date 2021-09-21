7cb39c1f00023a150f5d01e86d3725088605967e
GabrielVGS <75582525+GabrielVGS@users.noreply.github.com>
2021-09-21 12:37:13 -0300

    interface.py

---
+++ interface.py
@@ -0,0 +1,13 @@
+from graphics import *
+from plot_proj import *
+
+
+
+def main():
+    win = GraphWin("My Circle", 100, 100)
+    img = Image(Point(50,50),'function2d.png')
+    img.draw(win)
+    win.getMouse() # Pause to view result
+    win.close()    # Close window when done
+
+main()

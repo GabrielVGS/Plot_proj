df8c0725deb91943444907a981a0b9c320e03e67
GabrielVGS <75582525+GabrielVGS@users.noreply.github.com>
2021-09-23 20:55:44 -0300

    Update plot_proj.py

--- plot_proj.py
+++ plot_proj.py
@@ -10,11 +10,9 @@ def to_expression(expr):
 def plot_2dim(express):
     '''Plota uma função em 2d e salva ela no computador,
     Exemplo: x**2'''
-    try:
-        graph2d = plot(express,show = False)
-        graph2d.save("function2d.png")
-    except:
-        return "Expressão inválida"
+    graph2d = plot(express,show = False,xlim=[-10,10],ylim=[-10,10])
+    graph2d.save("function2d.png")
+        #return "Expressão inválida"
 
 def plot_3dim(express):
     '''Plota uma função em 3d e salva ela no computador,
@@ -31,8 +29,8 @@ def derivada(express):
     try:
         d = Derivative(express)
         deriv = d.doit()
-        func = plot(express,show = False)
-        derivada_img = plot(deriv,show = False,line_color='red')
+        func = plot(express,show = False,xlim=[-10,10],ylim=[-10,10])
+        derivada_img = plot(deriv,show = False,line_color='red',xlim=[-10,10],ylim=[-10,10])
         func.extend(derivada_img)
         func.save("deriv.png")
     except:

7056d6fa02bbe4622532868c8a5ce762e02fa7ab
GabrielVGS <75582525+GabrielVGS@users.noreply.github.com>
2021-09-28 12:40:10 -0300

    Update plot_proj.py

--- plot_proj.py
+++ plot_proj.py
@@ -1,4 +1,5 @@
 import sympy as sy
+import matplotlib
 from sympy.plotting import plot,plot3d
 from sympy import Derivative
 
@@ -12,26 +13,21 @@ def plot_2dim(express):
     Exemplo: x**2'''
     graph2d = plot(express,show = False,xlim=[-10,10],ylim=[-10,10])
     graph2d.save("function2d.png")
-        #return "Expressão inválida"
 
 def plot_3dim(express):
     '''Plota uma função em 3d e salva ela no computador,
     Exemplo: x**2 + y*x
     Exemplo2: (x*y)**2'''
-    try:
-        graph3d = plot3d(express,show = False)
-        graph3d.save('function3d.png')
-    except:
-        return "Expressão inválida"
+    graph3d = plot3d(express,show = False)
+    graph3d.save('function3d.png')
 
 
 def derivada(express):
-    try:
-        d = Derivative(express)
-        deriv = d.doit()
-        func = plot(express,show = False,xlim=[-10,10],ylim=[-10,10])
-        derivada_img = plot(deriv,show = False,line_color='red',xlim=[-10,10],ylim=[-10,10])
-        func.extend(derivada_img)
-        func.save("deriv.png")
-    except:
-        return "Expressão inválida"
+    d = Derivative(express)
+    deriv = d.doit()
+    func = plot(express,show = False,xlim=[-10,10],ylim=[-10,10],label="f(x)",legend=True)
+    derivada_img = plot(deriv,show = False,line_color='red',xlim=[-10,10],ylim=[-10,10],label="Derivada",legend=True)
+    func.extend(derivada_img)
+    func.save("deriv.png")
+
+
